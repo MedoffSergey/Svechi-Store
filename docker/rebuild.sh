5c2e68b1822ba3cc1294d781228ceabcaafb9416
MedoffSergey <mrgserega@mail.ru>
2020-01-18 19:59:29 +0300

    rebuild add

---
+++ docker/rebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$1" == "svechi" ]; then
+cd /docker/projects/svechi.store
+docker rm -f svechi
+docker rmi -f svechi
+docker build -t svechi .
+docker run -d --name svechi -p 10001:5000 -p 10002:3000 svechi
+fi
