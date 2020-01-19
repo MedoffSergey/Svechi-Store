647bd68a22b48a0191ec227349c23d97e9ac5dfd
MedoffSergey <mrgserega@mail.ru>
2020-01-19 20:38:46 +0300

    reconfig configs and resize images

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -5,5 +5,5 @@ cd /docker/projects/svechi.store
 docker rm -f svechi
 docker rmi -f svechi
 docker build -t svechi .
-docker run -d --name svechi -p 10001:5000 -p 10002:3000 svechi
+docker run -d --name svechi -p 10001:80 -p 10002:3000 svechi
 fi
